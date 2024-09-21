4e0b807c422c4f4ea1146233939458320bf2b1d0
Nirmal Aditya <140899243+Nirmal-aditya@users.noreply.github.com>
2024-09-21 22:01:52 +0530

    Update app.js

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const multerconfig=require("./config/multerconfig");
 const upload = require('./config/multerconfig');
 
 app.set("view engine","ejs")
+app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.use(express.json())
